8d6642ff7fe00088aa2729e8284fd950e4d7c254
leedahoon <honee8583@naver.com>
2022-05-17 10:12:05 +0900

    페이지용 dto 생성

---
+++ src/main/java/com/shoppingmall/shop/dto/PageRequestDTO.java
@@ -0,0 +1,29 @@
+package com.shoppingmall.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class PageRequestDTO {   //화면페이지에서 요청하는 데이터에 대한 정보
+
+    private int page;
+    private int size;
+    //private String type;
+    //private String keyword;
+
+    public PageRequestDTO(){
+        this.page = 1;
+        this.size = 10;
+    }
+
+    public Pageable getPageable(Sort sort){
+        return PageRequest.of(page -1, size, sort);
+    }
+
+}
---
+++ src/main/java/com/shoppingmall/shop/dto/PageResultDTO.java
@@ -0,0 +1,44 @@
+package com.shoppingmall.shop.dto;
+
+import lombok.Data;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Data
+public class PageResultDTO<DTO, EN> {
+
+    private List<DTO> dtoList;
+
+    private int totalPage;
+    private int page;
+    private int size;
+    private int start, end;
+    private boolean prev, next;
+    private List<Integer> pageList;
+
+    //result : service에서 가져온 Page데이터, fn : 가져온 page 데이터를 가공할 Function
+    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn){
+        dtoList = result.stream().map(fn).collect(Collectors.toList());
+        totalPage = result.getTotalPages();
+        makePageList(result.getPageable());
+    }
+
+    private void makePageList(Pageable pageable){
+        this.page = pageable.getPageNumber() + 1;
+        this.size = pageable.getPageSize();
+
+        int tempEnd = (int)(Math.ceil(page/10.0)) * 10;
+
+        start = tempEnd - 9;
+        prev = start > 1;
+        end = totalPage > tempEnd ? tempEnd : totalPage;
+        next = totalPage > tempEnd;
+
+        pageList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+    }
+}
